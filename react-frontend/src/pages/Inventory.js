fd6a662b4db10ad2548eccb73371df5507e1ba9d
copem1 <copem@calpoly.edu>
2023-11-01 11:13:26 -0700

    Inventory Editable Table

--- react-frontend/src/pages/Inventory.js
+++ react-frontend/src/pages/Inventory.js
@@ -1,9 +1,78 @@
+import React from "react";
+import "./css/inventory.css";
+import Table from "../Table";
+
+
 function Inventory() {  
   return (
     <div className="container">
-      <h1>inventory</h1>
-    </div>
+        <div class="topnav">
+        <a href="">Home</a>
+        <a class="active" href="#inventory">Inventory</a>
+        <a href="">History</a>
+        <a href="">Recipes</a>
+        <a href=""><span>&#9881;</span></a>
+      </div>
+      <div className="container">
+        <Table />
+      </div>
+
+      <div class="column">
+        <div class="column">
+          <a href=""><button class="cc-button">Custom Food Category</button></a>
+          <a href=""><button class="ei-button">Edit Inventory</button></a>
+          </div>
+          </div>
+        </div>
   )
 }
+
+// function TableHeader() {
+//   return (
+//     <thead>
+//       <tr>
+//         <th>ID</th>
+//         <th>Name</th>
+//         <th>Job</th>
+//         <th>Remove</th>
+//       </tr>
+//     </thead>
+//   );
+// }
+
+// function TableBody(props) {
+//   const rows = props.characterData.map((row, index) => {
+//     return (
+//       <tr key={index}>
+//         <td>{row._food}</td>
+//         <td>{row.quantitypurchased}</td>
+//         <td>{row.datepurchased}</td>
+//         <td>{row.quantityleft}</td>
+//         <td>
+//           <button onClick={() => 
+//             props.removeCharacter(index)}>
+//             Delete
+//           </button>
+//         </td>
+// 	    </tr>
+//     );
+//    }
+//   );
+//   return (
+//       <tbody>
+//         {rows}
+//        </tbody>
+//    );
+// }
+
+// function Table (props) {
+//   return (
+//     <table>
+//       <Inventory />
+//       <TableBody characterData={props.characterData} 
+// 	      removeCharacter={props.removeCharacter} />
+//     </table>
+//   );
+// }
 
 export default Inventory;

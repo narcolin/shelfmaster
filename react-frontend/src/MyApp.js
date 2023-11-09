6553b0840bdf79e8fd595a58dd238988c34af090
dphun123 <dennisphun321@gmail.com>
2023-11-08 21:57:43 -0800

    fixed flashing redirect, cleaned up authorization

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -10,7 +10,17 @@ import { supabase } from "./Client";
 
 function MyApp() {
   const location = useLocation();
-  const [token, setToken] = useState(false);
+
+  const [token, setToken] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
+
+  const titleMap = {
+    "/login": "Login - Shelf Master",
+    "/register": "Register - Shelf Master",
+    "/inventory": "Inventory - Shelf Master",
+    "/history": "History - Shelf Master",
+    "/recipes": "Recipes - Shelf Master",
+  };
 
   useEffect(() => {
     // updates the session storage if log in/out
@@ -31,52 +41,41 @@ function MyApp() {
           sessionStorage.getItem("token") || localStorage.getItem("token"),
         );
       }
+      // used to render blank screen so it doesn't have page flash
+      setAuthChecked(true);
     });
-
-    // sets title
-    document.title = "Shelf Master";
   }, []);
 
   // Updates the title whenever location changes
   useEffect(() => {
-    if (location.pathname === "/login") {
-      document.title = "Login - Shelf Master";
-    } else if (location.pathname === "/register") {
-      document.title = "Register - Shelf Master";
-    } else if (location.pathname === "/inventory") {
-      document.title = "Inventory - Shelf Master";
-    } else if (location.pathname === "/history") {
-      document.title = "History - Shelf Master";
-    } else if (location.pathname === "/recipes") {
-      document.title = "Recipes - Shelf Master";
-    } else {
-      document.title = "Shelf Master";
-    }
+    document.title = titleMap[location.pathname] || "Shelf Master";
   }, [location.pathname]);
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} exact />
-      <Route
-        path="/login"
-        element={token ? <Navigate to="/inventory" /> : <Login />}
-      />
-      <Route
-        path="/register"
-        element={token ? <Navigate to="/inventory" /> : <Register />}
-      />
-      <Route
-        path="/inventory"
-        element={token ? <Inventory /> : <Navigate to="/login" />}
-      />
-      <Route
-        path="/history"
-        element={token ? <History /> : <Navigate to="/history" />}
-      />
-      <Route
-        path="/recipes"
-        element={token ? <Recipes /> : <Navigate to="/recipes" />}
-      />
+      {authChecked ? (
+        token ? (
+          <>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/login" element={<Navigate to="/inventory" />} />
+            <Route path="/register" element={<Navigate to="/inventory" />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/recipes" element={<Recipes />} />
+          </>
+        ) : (
+          <>
+            <Route path="/" element={<Home />} exact />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/inventory" element={<Navigate to="/login" />} />
+            <Route path="/history" element={<Navigate to="/login" />} />
+            <Route path="/recipes" element={<Navigate to="/login" />} />
+          </>
+        )
+      ) : (
+        <Route path={location.pathname} element={<></>} />
+      )}
     </Routes>
   );
 }

4efef2aa52fe28a1daba4fdf619c4330e6445c32
narcolin <narcolin@calpoly.edu>
2023-11-08 22:18:27 -0800

    settings dropdown

--- react-frontend/src/components/Menu.js
+++ react-frontend/src/components/Menu.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import logo from "../images/logo.png";
 
 const Menu = ({ active }) => {
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  const closeDropdown = () => {
+    setDropdownVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", closeDropdown);
+    return () => {
+      window.removeEventListener("click", closeDropdown);
+    };
+  }, []);
+
   return (
-    <div className="topnav">
+    <div className="navbar">
+      <img className="logoimg" src={logo} width={250} height={40}></img>
       <a className={active === "Home" ? "active" : ""} href="/">
         Home
       </a>
@@ -15,9 +34,26 @@ const Menu = ({ active }) => {
       <a className={active === "Recipes" ? "active" : ""} href="/recipes">
         Recipes
       </a>
-      <a href="">
-        <span>&#9881;</span>
-      </a>
+      <div className="dropdown" onClick={toggleDropdown}>
+        <button className="dropbtn">Settings &nbsp;&#x25BC;</button>
+        <div className="dropdown-content" id="myDropdown">
+          <a
+            className={active === "Account Settings" ? "active" : ""}
+            href="/settings"
+          >
+            Account Settings
+          </a>
+          <a className={active === "About" ? "active" : ""} href="/about">
+            About
+          </a>
+          <a
+            className={active === "Give Feedback" ? "active" : ""}
+            href="/feedback"
+          >
+            Give Feedback
+          </a>
+        </div>
+      </div>
     </div>
   );
 };

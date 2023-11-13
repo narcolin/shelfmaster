4f37050cff145785936df921dc41762f7504522e
narcolin <narcolin@calpoly.edu>
2023-11-13 11:37:20 -0800

    add p to home page

--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -9,15 +9,11 @@ function Home() {
         <strong>ShelfMaster</strong>
       </h1>
       <p>
-        Use ShelfMaster to keep track of what food items you have.
-        <br />
-        <br />
-        Visit the Inventory tab to view what you have, or add or delete items.
-        <br />
-        View what you’ve used or eaten in the History tab.
-        <br />
-        The Recipes tab helps you curate recipe ideas based off what’s in your
-        inventory.
+        ShelfMaster is for anyone and everyone who needs to keep track of their
+        meals and make sure they have enough food to last. ShelfMaster is an
+        inventory site that allows users to easily add or remove items from
+        their tally. This product creatively formats your pantry items into food
+        group categories, like grains, proteins, vegetables, etc.
         <br />
         <br />
       </p>
@@ -29,7 +25,18 @@ function Home() {
           <a href="/login">
             <button className="gs-button">Get Started</button>
           </a>
+          <br />
         </div>
+        <p>
+          Visit the Inventory tab to view what you have, or add or delete items.
+          <br />
+          View what you’ve used or eaten in the History tab.
+          <br />
+          The Recipes tab helps you curate recipe ideas based off what’s in your
+          inventory.
+          <br />
+          <br />
+        </p>
       </div>
     </div>
   );

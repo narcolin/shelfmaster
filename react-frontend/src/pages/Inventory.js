42b4aae848a19fa7bd9bb0559de083cca28f4255
dphun123 <dennisphun321@gmail.com>
2023-11-17 10:40:37 -0800

    fix inventory error

--- react-frontend/src/pages/Inventory.js
+++ react-frontend/src/pages/Inventory.js
@@ -1,28 +1,8 @@
 // import React from "react";
-import React, { useState } from "react";
 import Table from "../Table";
 import InventoryDrawer from "../components/InventoryDrawer.js";
 
-import IconButton from "@mui/icons-material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-
 function Inventory() {
-  const [drawerOpen, setDrawerOpen] = useState(false);
-
-  const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  <IconButton
-    edge="start"
-    color="inherit"
-    aria-label="menu"
-    onClick={toggleDrawer}
-    style={{ position: "absolute", left: 0, top: 0 }}
-  >
-    <MenuIcon />
-  </IconButton>;
-
   return (
     <div>
       <div className="container">
@@ -30,7 +10,6 @@ function Inventory() {
           ShelfMaster <strong>Inventory</strong>
         </h1>
         <InventoryDrawer />
-        {/* <Drawer open={drawerOpen} /> */}
         <br />
         <Table />
       </div>
